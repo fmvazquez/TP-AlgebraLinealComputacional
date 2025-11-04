a5b1430bc64a22bf425044ca8804714890e1e00e
Nicolas <demomantf2@tf2.com>
2025-11-04 13:21:07 -0300

    LU

--- alc.py
+++ alc.py
@@ -12,42 +12,41 @@ def norma(x, p):
 
 # Labo04
 def calculaLU(A):
+    cant_op = 0
+    m=A.shape[0]
+    n=A.shape[1]
+    Ac = A.copy()
+
     """
     Calcula la factorización LU de la matriz A y retorna las matrices L y U, 
     junto con el número de operaciones realizadas. 
 
     En caso de que la matriz no pueda factorizarse, retorna None.
     """
-
-    cant_op = 0
-    M=A.shape[0]
-    N=A.shape[1]
-    Ac = A.copy()
     
-    if M!=N:
+    if m!=n:
         print('Matriz no cuadrada')
         return
     
-    for i in range(N):
-        piv = Ac[i][i]
-        for j in range(i+1, N):
-            
-            mult = Ac[j][i] / piv
-            cant_op+=1
-            for k in range(i, N):
-                Ac[j][k] -= mult * Ac[i][k]
-                cant_op+=2
-            Ac[j][i] = mult
-
-    L = np.eye(N,N)
-    U = np.eye(N,N)
-
-    for i in range(N):
-        for j in range(N):
-            if (i <= j): U[i, j] = Ac[i, j]
-            else: L[i, j] = Ac[i, j]
-
+    L = np.zeros((m,m))
+    U = np.zeros((m,m))
+    for k in range (m):
+        pivot = Ac[k,k]
+        if pivot == 0: return None, None, 0
+        L[k,k] = 1
+        U[k,k] = Ac[k,k]
+        for i in range(k+1,m):
+            Ac[i,k] = Ac[i,k]/pivot
+            L[i,k] = Ac[i,k]
+            U[i,k] = 0
+            cant_op += 1
+            for j in range (k+1,m):
+                if i == 1: U[i-1,j] = Ac[i-1,j]
+                Ac[i,j] = Ac[i,j] - Ac[i,k]*Ac[k,j]
+                U[i,j] = Ac[i,j]
+                cant_op += 2
     return L, U, cant_op
+
 
 def res_tri(L,b,inferior=True):
     """
