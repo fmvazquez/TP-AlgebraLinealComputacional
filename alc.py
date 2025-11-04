7b009834a8ac0cfa377d9ff8a30d30ab3d9f445f
fmvazquez <facundo.vazber@gmail.com>
2025-11-04 13:46:51 -0300

    aa

--- alc.py
+++ alc.py
@@ -1,5 +1,52 @@
 import numpy as np
 
+def prodMat(A, B):
+    if A.shape[1] != B.shape[0]:
+        print("No se puede hacer el producto matricial")
+        return
+    res = np.zeros((A.shape[0], B.shape[1]))
+
+    for i in range(res.shape[0]):
+        for j in range(res.shape[1]):
+            tot = 0
+            for k in range(A.shape[1]):
+                tot += A[i, k] * B[k, j]
+            res[i, j] = tot
+
+    return res
+
+def aplicTrans(A, v):
+    """
+    Asume que el vector es columna
+    """
+
+    if A.shape[1] != v.shape[0]:
+        print("No se puede aplicar la matriz al vector")
+        return
+    res = np.zeros((A.shape[0]))
+
+    for i in range(res.shape[0]):
+        tot = 0            
+        for k in range(A.shape[1]):
+            tot += A[i, k] * v[k]
+        res[i] = tot
+
+    return res
+
+def prodPunto(v, w):
+    """
+    Asume que el vector es columna
+    """
+
+    if v.shape[0] != w.shape[0]:
+        print("No se puede calcular el prodcuto punto")
+        return
+    
+    res = 0
+    for k in range(v.shape[0]):
+        res += v[k] * w[k]
+
+    return res
 
 # Labo03
 def norma(x, p):
@@ -12,40 +59,46 @@ def norma(x, p):
 
 # Labo04
 def calculaLU(A):
-    cant_op = 0
-    m=A.shape[0]
-    n=A.shape[1]
-    Ac = A.copy()
-
     """
     Calcula la factorización LU de la matriz A y retorna las matrices L y U, 
     junto con el número de operaciones realizadas. 
 
     En caso de que la matriz no pueda factorizarse, retorna None.
     """
+
+    cant_op = 0
+    M=A.shape[0]
+    N=A.shape[1]
+    Ac = A.copy()
     
-    if m!=n:
+    if M!=N:
         print('Matriz no cuadrada')
         return
     
-    L = np.zeros((m,m))
-    U = np.zeros((m,m))
-    for k in range (m):
-        pivot = Ac[k,k]
-        if pivot == 0: return None, None, 0
-        L[k,k] = 1
-        U[k,k] = Ac[k,k]
-        for i in range(k+1,m):
-            Ac[i,k] = Ac[i,k]/pivot
-            L[i,k] = Ac[i,k]
-            U[i,k] = 0
-            cant_op += 1
-            for j in range (k+1,m):
-                if i == 1: U[i-1,j] = Ac[i-1,j]
-                Ac[i,j] = Ac[i,j] - Ac[i,k]*Ac[k,j]
-                U[i,j] = Ac[i,j]
-                cant_op += 2
+    for i in range(N):
+        piv = Ac[i][i]
+        for j in range(i+1, N):
+            
+            mult = Ac[j][i] / piv
+            cant_op+=1
+            for k in range(i, N):
+                Ac[j][k] -= mult * Ac[i][k]
+                cant_op+=2
+            Ac[j][i] = mult
+    
+    print(Ac)
+
+    L = np.eye(N,N)
+    U = np.eye(N,N)
+
+    for i in range(N):
+        for j in range(N):
+            if (i <= j): U[i, j] = Ac[i, j]
+            else: L[i, j] = Ac[i, j]
+
     return L, U, cant_op
+
+
 
 
 def res_tri(L,b,inferior=True):
@@ -74,3 +127,94 @@ def res_tri(L,b,inferior=True):
             if i == j: res[i] /= L[i, i]
             else: res[i] -= L[i, j] * res[j]    
     return res
+
+# Labo06
+def metpot2k(A, tol=1e-15, K=1000):
+    """
+    A: matriz de tamaño n x n
+    tol: tolerancia en la diferencia entre un paso y el siguiente de la estimación del autovector
+    K: número máximo de iteraciones a realizar
+
+    Retorna:
+    v: autovector estimado
+    lambda: autovalor estimado
+    k: número de iteraciones realizadas
+    """
+
+    N = A.shape[0]
+
+    v = np.random.rand(N)
+    v = v / norma(v,2)
+
+    v_monio = aplicTrans(A, v) 
+    v_monio = v_monio / norma(v_monio,2)
+
+    e = prodPunto(v_monio, v)
+    k = 0
+    while abs(e - 1) > tol and k < K:
+        v = v_monio
+
+        # Aplicar fA dos veces sobre v
+        w = aplicTrans(A, v)
+        w = w / norma(w, 2)
+
+        v_monio = aplicTrans(A, w)
+        v_monio = v_monio / norma(v_monio, 2)
+
+        e = prodPunto(v_monio, v)
+        k += 1
+    
+    v_monio = np.expand_dims(v_monio, axis=0).T
+
+    lambd = prodMat(prodMat(v_monio.T, A), v_monio)[0][0]
+    eps = e-1
+
+    return v_monio, lambd, eps
+
+def diagRH(A, tol=1e-15, K=1000):
+    """
+    A: Matriz simétrica de tamaño n x n
+    tol: Tolerancia en la diferencia entre un paso y el siguiente de la estimación del autovector
+    K: Número máximo de iteraciones a realizar
+
+    Retorna:
+    - Matriz de autovectores S
+    - Matriz de autovalores D tal que A = S D S.T
+    
+    Si la matriz A no es simétrica, debe retornar None
+    """
+
+    if (A.shape[0] != A.shape[1]):
+        print("Matriz no cuadrada")
+        return
+    
+    N = A.shape[0]
+
+
+    v1, lambda1, _ = metpot2k(A, tol, K)
+    e1 = np.zeros((N,1))
+    e1[0][0] = 1
+
+
+    factor = 2 / (norma(np.squeeze(e1 - v1),2)**2)
+    Hv1 = np.eye(N) - factor * prodMat(e1-v1, (e1-v1).T)
+    if N == 2:
+        S = Hv1
+        D = prodMat(prodMat(Hv1, A), Hv1.T)
+    else: 
+        B = prodMat(prodMat(Hv1, A), Hv1.T)
+        Ahat = B[1:N,1:N]
+        Shat, Dhat = diagRH(Ahat, tol=1e-15, K=1000)
+        D = np.zeros((N, N))
+        D[0][0] = lambda1
+        for i in range(1,N):
+            for j in range(1,N):
+                D[i][j] = Dhat[i-1][j-1]
+        Hprod = np.zeros((N, N))
+        Hprod[0][0] = 1
+        for i in range(1,N):
+            for j in range(1,N):
+                Hprod[i][j] = Shat[i-1][j-1]
+        S = prodMat(Hv1, Hprod)
+    
+    return S, D
---
+++ facu.py
@@ -0,0 +1,29 @@
+from alc import calculaLU
+import numpy as np
+
+def calculaCholesky(A,atol=1e-10):
+    N = A.shape[0]    
+    res = calculaLU(A)
+    if (res == None):
+        print("No se puede hacer Cholesky, no existe desc LU")
+        return
+    L, U, _ = res
+    print(A)
+    print(L)
+    print(U)
+    D = np.zeros((N,N))
+    for i in range(N):
+        D[i,i] = U[i,i]
+        for j in range(i, N):
+            U[i,j] = U[i,j] / U[i,i]
+
+A = np.array([
+    [25,15,-5],
+    [15,18,0],
+    [-5,0,11]
+])
+
+calculaCholesky(A)
+
+
+
